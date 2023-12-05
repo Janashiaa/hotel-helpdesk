e12aee125c5684c3a7e1bd3da367ac0136c893ec
janashiaa <davitjanashia3@gmail.com>
2023-12-05 22:28:50 +0400

    Styled sign in page and added wrong input alert

--- mainscript.js
+++ mainscript.js
@@ -1,10 +1,11 @@
 let username = document.querySelector(".username_input");
 let password = document.querySelector(".password_input");
+let alert = document.querySelector(".wrong_alert_box");
 
 function check_auth(){
     if (username.value === "davitjanashia3@gmail.com" && password.value === "12345678") {
         window.location.href = "../ReceptionBar/recept.html"
     } else {
-        window.location.href = "index.html"
+        alert.style.display = "flex";
     }
 }
