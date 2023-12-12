1df01de12f0a627813cbbc04d228f183cbb7ae36
janashiaa <davitjanashia3@gmail.com>
2023-12-13 00:27:22 +0400

    style changed

--- mainscript.js
+++ mainscript.js
@@ -1,11 +1,20 @@
 let username = document.querySelector(".username_input");
 let password = document.querySelector(".password_input");
 let alert = document.querySelector(".wrong_alert_box");
+let testBox = document.querySelector(".test_shower_box")
 
-function check_auth(){
-    if (username.value === "davitjanashia3@gmail.com" && password.value === "12345678") {
+function check_auth() {
+    if (username.value === "watcher" && password.value === "1234") {
         window.location.href = "../ReceptionBar/recept.html"
     } else {
         alert.style.display = "flex";
     }
+}
+
+function main_card_mouse_over() {
+    testBox.style.display = "block";
+}
+
+function main_card_mouse_out() {
+    testBox.style.display = "none"
 }
